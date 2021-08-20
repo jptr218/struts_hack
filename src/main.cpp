fceedc407ab93d25a7e093ac382ee1fb787542b1
jptr218 <84092766+jptr218@users.noreply.github.com>
2021-08-20 10:56:56 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include "hdr.h"
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        cout << "Usage:" << endl << "struts_hack [target] [port]" << endl;
+    if (argc != 4) {
+        cout << "Usage:" << endl << "struts_hack [target] [port] [servlet]" << endl;
         return 1;
     }
 
@@ -13,7 +13,7 @@ int main(int argc, char* argv[]) {
     }
 
     cout << "Gaining access..." << endl;
-    telnet(addr, argv[1]);
+    telnet(addr, argv[1], argv[3]);
 
     return 1;
 }
